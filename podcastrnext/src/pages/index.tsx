6d3671cb46ec4249476e0e0d0dbea7683b114f1e
Roni <roni.c.castro@gmail.com>
2021-04-21 18:00:34 -0300

    feat: make home page static and revalidate every 8h

--- podcastrnext/src/pages/index.tsx
+++ podcastrnext/src/pages/index.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
-export default function Home() {
-  return <div>Home</div>;
+export default function Home({ episodes }) {
+
+  return <div>{JSON.stringify(episodes)}</div>;
+}
+
+export async function getStaticProps() {
+  try {
+    const response = await fetch('http://localhost:3333/episodes');
+    const data = await response.json();
+    return {
+      props: {
+        episodes: data
+      },
+      revalidate: 60 * 60 * 8
+    };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
