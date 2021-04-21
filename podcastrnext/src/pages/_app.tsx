c82ff009f9b4fdf3f472b6cbe45332e3343e7993
Roni <roni.c.castro@gmail.com>
2021-04-21 16:21:33 -0300

    feat: add header as a global component

--- podcastrnext/src/pages/_app.tsx
+++ podcastrnext/src/pages/_app.tsx
@@ -1,7 +1,17 @@
+import { Header } from '../components/Header';
+import React from 'react';
 import '../styles/global.css';
+import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
 
 export default MyApp;
